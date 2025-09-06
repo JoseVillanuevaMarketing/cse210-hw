24562d42ca9580146688dc73d57f46acf5fb7bab
JoseVillanuevaMarketing <skymarkdigitalba@gmail.com>
2025-09-06 14:47:11 -0400

    e4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,78 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int userNumber = -1;
+
+        
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+    
+        double average = (double)sum / numbers.Count;
+
+        
+        int max = numbers[0];
+        foreach (int num in numbers)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+
+      
+        int? smallestPositive = null;
+        foreach (int num in numbers)
+        {
+            if (num > 0)
+            {
+                if (smallestPositive == null || num < smallestPositive)
+                {
+                    smallestPositive = num;
+                }
+            }
+        }
+
+        
+        numbers.Sort();
+
+       
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
+
+        if (smallestPositive != null)
+        {
+            Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+        }
+        else
+        {
+            Console.WriteLine("There are no positive numbers.");
+        }
+
+        Console.WriteLine("The sorted list is:");
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
