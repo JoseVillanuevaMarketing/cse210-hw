bd1099495ce661a1ccd44cdab97a7dd117c9c0e9
JoseVillanuevaMarketing <skymarkdigitalba@gmail.com>
2025-09-06 14:39:18 -0400

    e3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -7,7 +7,7 @@ class Program
         Console.Write("What is the magic number? ");
         int magicNumber = int.Parse(Console.ReadLine());
 
-        int guess = -1; // valor inicial cualquiera
+        int guess = -1; 
         while (guess != magicNumber)
         {
             Console.Write("What is your guess? ");
