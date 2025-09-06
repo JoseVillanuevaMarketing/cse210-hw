5907b85c63e44c383b9522e12e3394f63777e668
JoseVillanuevaMarketing <skymarkdigitalba@gmail.com>
2025-09-06 13:32:06 -0400

    w1

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -7,3 +7,8 @@ class Program
         Console.WriteLine("Hello World! This is the Sandbox Project.");
     }
 }
+first_name = input("What is your first name? ")
+
+last_name = input("What is your last name? ")
+
+print(f"Your name is {last_name}, {first_name} {last_name}.")
