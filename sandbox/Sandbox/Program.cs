b99775801ba13c26a51f059d99ed0f0b62afac8e
JoseVillanuevaMarketing <skymarkdigitalba@gmail.com>
2025-09-06 13:36:48 -0400

    w1

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -7,21 +7,3 @@ class Program
         Console.WriteLine("Hello World! This is the Sandbox Project.");
     }
 }
-using System;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        // Pedir primer nombre
-        Console.Write("What is your first name? ");
-        string firstName = Console.ReadLine();
-
-        // Pedir apellido
-        Console.Write("What is your last name? ");
-        string lastName = Console.ReadLine();
-
-        // Mostrar resultado
-        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
-    }
-}
